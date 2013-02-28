35cf5263e80a6c98e6bc9a76bbf1ec91c0533799
Benjamin Gehrels <github@gehrels>
2013-02-28 02:14:06 +0100

    whatever happened here - git seems to see some differences

--- src/main/java/info/gehrels/parameterValidation/MatcherValidation.java
+++ src/main/java/info/gehrels/parameterValidation/MatcherValidation.java
@@ -1,4 +1,24 @@
 package info.gehrels.parameterValidation;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+
 public class MatcherValidation {
+ public static <T> void validateThat(T actual, Matcher<? super T> matcher) {
+     validateThat("", actual, matcher);
+ }
+
+ public static <T> void validateThat(String reason, T actual, Matcher<? super T> matcher) {
+     if (!matcher.matches(actual)) {
+         Description description = new StringDescription();
+         description.appendText(reason)
+                    .appendText("\nExpected: ")
+                    .appendDescriptionOf(matcher)
+                    .appendText("\n     but: ");
+         matcher.describeMismatch(actual, description);
+
+         throw new IllegalArgumentException(description.toString());
+     }
+ }
 }
--- src/test/java/info/gehrels/parameterValidation/MatcherValidationTest.java
+++ src/test/java/info/gehrels/parameterValidation/MatcherValidationTest.java
@@ -1,4 +1,72 @@
 package info.gehrels.parameterValidation;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import static info.gehrels.parameterValidation.MatcherValidation.validateThat;
+import static junit.framework.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MatcherValidationTest {
+	@Test
+	public void throwsIllegalArgumentExceptionWithoutCustomMessageWhenMatcherDoesNotMatch() {
+		try {
+			validateThat("arbitraryString", matcherStub(false));
+		} catch (IllegalArgumentException e) {
+			assertThat(e.getMessage(), is("\nExpected: full description"
+			                              + "\n     but: mismatch description"));
+			return;
+		}
+
+		fail("IllegalArgumentException has not been thrown");
+	}
+
+	@Test
+	public void throwsIllegalArgumentExceptionWithCustomMessageWhenMatcherDoesNotMatch() {
+		try {
+			validateThat("arbitrary message", "arbitrary object", matcherStub(false));
+		} catch (IllegalArgumentException e) {
+			assertThat(e.getMessage(), is("arbitrary message"
+			                              + "\nExpected: full description"
+			                              + "\n     but: mismatch description"));
+			return;
+		}
+
+		fail("IllegalArgumentException has not been thrown");
+	}
+
+	@Test
+	public void doesNotThrowIllegalArgumentExceptionIfMatcherMatches() {
+	    validateThat("arbitrary object", matcherStub(true));
+	}
+
+
+	private Matcher<? super String> matcherStub(final boolean matches) {
+		return new Matcher<Object>() {
+
+			@Override
+			public boolean matches(Object o) {
+				return matches;
+			}
+
+			@Override
+			public void describeMismatch(Object o, Description description) {
+				description.appendText("mismatch description");
+			}
+
+			@Override
+			public void _dont_implement_Matcher___instead_extend_BaseMatcher_() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public void describeTo(Description description) {
+				description.appendText("full description");
+			}
+		};
+	}
+
+
 }
