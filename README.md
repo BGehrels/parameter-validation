9e3415124c15c3aa9add8a0a7fff7ee15b0207ef
Benjamin Gehrels <github@gehrels>
2013-03-01 21:26:31 +0100

    Added Licensing info

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ parameter-validation
 ====================
 
 Validating input parameters of methods and constructors is a recurring and stupid task, especially when you are writing public APIs. This module tries to ease your life a bit by providing a static validateThat() method, allowing you to throw IllegalArgumentExceptions using hamcrest matchers.
+
+Using the matchers is as easy as
+
+	validateThat(name, not(isEmptyOrNullString()));
+
+
+or
+
+    validateThat("parameter 'name has an invalid value", name, not(isEmptyOrNullString()));
+
+If it fails to validate the parameter, an Exception is thrown:
+    Exception in thread "main" java.lang.IllegalArgumentException: parameter 'name has an invalid value
+    Expected: not (null or an empty string)
+         but: was null
+    	at info.gehrels.parameterValidation.MatcherValidation.validateThat(MatcherValidation.java:40)
+    	...
+
--- src/main/java/info/gehrels/parameterValidation/MatcherValidation.java
+++ src/main/java/info/gehrels/parameterValidation/MatcherValidation.java
@@ -4,21 +4,40 @@ import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.StringDescription;
 
-public class MatcherValidation {
- public static <T> void validateThat(T actual, Matcher<? super T> matcher) {
-     validateThat("", actual, matcher);
- }
+/**
+ * Contains static methods to validate parameters using Hamcrest matchers.
+ */
+public final class MatcherValidation {
+	private MatcherValidation() {
+		// To prevent instantiation.
+	}
+	/**
+	 * Throws an IllegalArgumentException with a meaningfull error message if the given value is not matched by the
+	 * given value. Has no side effects otherwise.
+	 * @param actual the value to validate
+	 * @param matcher the matcher used to validate the actual value
+	 */
+	public static <T> void validateThat(T actual, Matcher<? super T> matcher) {
+		validateThat("", actual, matcher);
+	}
 
- public static <T> void validateThat(String reason, T actual, Matcher<? super T> matcher) {
-     if (!matcher.matches(actual)) {
-         Description description = new StringDescription();
-         description.appendText(reason)
-                    .appendText("\nExpected: ")
-                    .appendDescriptionOf(matcher)
-                    .appendText("\n     but: ");
-         matcher.describeMismatch(actual, description);
+	/**
+	 * Throws an IllegalArgumentException with a meaningfull error message if the given value is not matched by the
+	 * given value. Has no side effects otherwise.
+	 * @param reason a String that is guarenteed to be contained in the message of the IllegalArgumentException
+	 * @param actual the value to validate
+	 * @param matcher the matcher used to validate the actual value
+	 */
+	public static <T> void validateThat(String reason, T actual, Matcher<? super T> matcher) {
+		if (!matcher.matches(actual)) {
+			Description description = new StringDescription();
+			description.appendText(reason)
+				.appendText("\nExpected: ")
+				.appendDescriptionOf(matcher)
+				.appendText("\n     but: ");
+			matcher.describeMismatch(actual, description);
 
-         throw new IllegalArgumentException(description.toString());
-     }
- }
+			throw new IllegalArgumentException(description.toString());
+		}
+	}
 }
